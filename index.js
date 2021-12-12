4f4fd9d7ea76370a7311df260ff4c0cff05ca6c2
Igor Chinchay <igor2008_11@hotmail.com>
2021-12-12 15:01:23 -0500

    Add messageGenerator with generateRandomNumber functions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello")
+import { quotes } from "./quotes.js";
+
+
+const generateRandomNumber = (length) => {
+    const randomNumber = Math.floor(Math.random() * (length));
+    return randomNumber;
+}
+
+const messageGenerator = () => {
+    const number1 = generateRandomNumber(quotes.length);
+    const number2 = generateRandomNumber(quotes.length);
+    const number3 = generateRandomNumber(quotes.length);
+    return quotes[number1].message + " " + quotes[number2].message + " " + quotes[number3].message;
+}
+
+console.log(messageGenerator());
---
+++ quotes.js
@@ -0,0 +1,32 @@
+export const quotes = [
+    {
+        id: 1,
+        message: "In my studies of astronomy and philosophy I hold this opinion about the universe, that the Sun remains fixed in the centre of the circle of heavenly bodies, without changing its place; and the Earth, turning upon itself, moves round the Sun.",
+        author: "Galileo Galilei"
+    },
+    {
+        id: 2,
+        message: "The contemplation of celestial things will make a man both speak and think more sublimely and magnificently when he descends to human affairs.",
+        author: "Marcus Tullius Cicero"
+    },
+    {
+        id: 3,
+        message: "Chances are, when we meet intelligent life forms in outer space they're going to be descended from predators.",
+        author: "Michio Kaku"
+    },
+    {
+        id: 4,
+        message: "Astronomy, as nothing else can do, teaches men humility.",
+        author: "Arthur C. Clarke"
+    },
+    {
+        id: 5,
+        message: "Everything has a natural explanation. The moon is not a God, but a great rock, and the sun a hot rock.",
+        author: "Anaxagoras"
+    },
+    {
+        id: 6,
+        message: "For astronomy is not only pleasant, but also very useful to be known: it cannot be denied that this art unfolds the admirable wisdom of God.",
+        author: "John Calvin"
+    }
+];
